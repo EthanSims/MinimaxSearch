9197acb7c50cc325232677fea2b7e50dd296339d
Ethan Sims <easims@mines.edu>
2023-02-16 11:48:53 -0700

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file main.cpp
+ * @author Ethan Sims (ea.sims@me.com)
+ * @brief handles command-line arguments and starts game
+ * @version 0.1
+ * @date 2023-02-16
+ */
+
+int main(int argc, char* argv[]) {
+   
+}
