f511bc6921ba175cdf86394765f10297476ce774
Ethan Sims <easims@mines.edu>
2023-02-22 10:49:11 -0700

    Implemented some functions for GameBoard. Constructor, getAvailMoves, getNumPieces, printBoard

--- includes/GameBoard.h
+++ includes/GameBoard.h
@@ -1,3 +1,9 @@
+/**
+ * @file GameBoard.h
+ * @author Ethan Sims (ea.sims@me.com)
+ * @brief header file for GameBoard object
+ * @date 2023-02-22
+ */
 #ifndef GAME_BOARD_H
 #define GAME_BOARD_H
 #include <vector>
@@ -6,20 +12,26 @@
 using namespace std;
 
 enum Pieces {
-   RED,
-   GREEN
+   RED = 'R',
+   GREEN = 'G'
 };
 
 class GameBoard {
    private:
       vector<Pieces> columns[7];
       int numPieces;
+      bool redNext;
+      int scores[2];
+
+      void getScores();
    public:
       GameBoard(ifstream&);
 
-      int* getAvailMoves();
+      vector<int> getAvailMoves();
 
       int getNumPieces();
+
+      void printBoard();
 };
 
 #endif
--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -1,4 +1,12 @@
-#include "GameBoard.h"
+/**
+ * @file GameBoard.cpp
+ * @author Ethan Sims (ea.sims@me.com)
+ * @brief contains information about the game
+ * @date 2023-02-22
+ */
+#include "../includes/GameBoard.h"
+
+#include <iostream>
 
 using namespace std;
 
@@ -8,10 +16,77 @@ using namespace std;
  * @param input filestream to read initial state from
  */
 GameBoard::GameBoard(ifstream &input) {
-   char currSlot;
+   int currSlot;
+   this->numPieces = 0;
    
+   // get main board input
    for (int row = 0 ; row < 6 ; row++) {
-      input >> currSlot;
+      for (int column = 0 ; column < 7 ; column++) {
+         // check if current slot is empty, red, or green
+         input >> currSlot;
+         if (currSlot == 0) { // empty
+            continue;
+         } else if (currSlot == 1){ // red
+            this->columns[column].at(5 - row) = Pieces::RED;
+            this->numPieces++;
+         } else { // green
+            this->columns[column].at(5 - row) = Pieces::GREEN;
+            this->numPieces++;
+         }
+      }
    }
 
+   input >> currSlot;
+   this->redNext = (currSlot == '1');
+
+   getScores();
+}
+
+/**
+ * @brief checks which moves are still possible
+ * 
+ * @return a vector of possible moves
+ */
+vector<int> GameBoard::getAvailMoves() {
+   vector<int> moves;
+   for (int column = 0 ; column < 7 ; column++) {
+      if (this->columns[column].size() < 6) {
+         moves.push_back(column);
+      }
+   }
+
+   return moves;
+}
+
+/**
+ * @brief getter for numPieces
+ * 
+ * @return the number of pieces on the board
+ */
+int GameBoard::getNumPieces() {
+   return this->numPieces;
+}
+
+/**
+ * @brief gets the scores for red and green (scores[0] is red, scores[1] is green)
+ */
+void GameBoard::getScores() {
+   // TODO: implement 
+}
+
+/**
+ * @brief prints the current board state
+ */
+void GameBoard::printBoard() {
+   for (int row = 0 ; row < 6 ; row++) {
+      for (int column = 0 ; column < 7 ; column++) {
+         if (this->columns[column].size() < 6 - row) { // this spot is empty
+            cout << "O ";
+         }
+         // not empty, print color
+         Pieces color = this->columns[column].at(5 - row);
+         cout << color << " ";
+      }
+      cout << '\n';
+   }
 }
